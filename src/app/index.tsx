9156b75a3cab8bd3155f06a445fe4e9fb098af21
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-03-29 17:33:08 -0300

    Navegação - Utilizando Botões com o Link

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,13 @@
-import { View, StyleSheet} from "react-native"
+import { View, StyleSheet, TouchableOpacity, Text} from "react-native"
 import { Link } from 'expo-router'
 
 export default function Index(){
   return(
     <View style={styles.constainer}>
-      <Link href="/sign-up" style={styles.new}>
-         Criar conta
+      <Link href="/sign-up" asChild >
+        <TouchableOpacity style={styles.button}>
+           <Text style={styles.label}>Criar Conta</Text>
+         </TouchableOpacity>
        </Link>
     </View>
   )
@@ -13,5 +15,11 @@ export default function Index(){
 
 const styles = StyleSheet.create({
   constainer: { flex: 1, justifyContent: "center", alignItems: "center" },
-  new: {fontSize: 16, fontWeight: "bold" }
+  label: {fontSize: 16, fontWeight: "bold", color: '#FFF', },
+  button: {
+    backgroundColor: '#000',
+    paddingHorizontal: 32,
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
 })
--- src/app/sign-up.tsx
+++ src/app/sign-up.tsx
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import { Link } from 'expo-router'
  
  export default function Index() {
    return (
      <View style={styles.container}>
-       <Text style={styles.title}>Criar conta</Text>
+       <Link href="/" style={styles.back}>
+        Voltar
+       </Link>
      </View>
    )
  }
  
  const styles = StyleSheet.create({
    container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-   title: { fontSize: 22, fontWeight: 'bold' },
+   back: { fontSize: 16, fontWeight: 'bold' },
  })
