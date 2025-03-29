54b2937a377237b606baab087cfbe1e1fd2c92cc
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-03-29 20:24:35 -0300

    Tabs e Drawer - Reorganizando a aplicação

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,37 +1,7 @@
-import { Drawer } from 'expo-router/drawer'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { MaterialIcons } from '@expo/vector-icons'
+import { Stack } from 'expo-router'
 
 export default function Layout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer screenOptions={{
-        drawerActiveTintColor: "#000000",
-        drawerInactiveTintColor: "#999999",
-        headerShown: false,
-      }}>
-       <Drawer.Screen 
-        name='index' 
-        options={{
-          title: "Entrar",
-          drawerLabel: "Entrar",
-          drawerIcon: ({ color, size }) => (
-            <MaterialIcons name='home' size={size} color={color} />
-          ),
-        }}
-        />
-
-        <Drawer.Screen 
-          name="sign-up"
-          options={{
-            title: "Criar Conta",
-            drawerLabel: "Criar Conta",
-            drawerIcon: ({ color, size }) => (
-              <MaterialIcons name='add' size={size} color={color} />
-            ),
-          }}
-          />
-      </Drawer>
-     </GestureHandlerRootView>
+    <Stack screenOptions={{ headerShown: false }} />
   )
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,5 @@
 import { View, StyleSheet, TouchableOpacity, Text} from "react-native"
 import { router } from 'expo-router'
-import { DrawerToggleButton } from '@react-navigation/drawer'
 
 export default function Index(){
   function signUp() {
@@ -9,10 +8,7 @@ export default function Index(){
 
   return(
     <View style={styles.constainer}>
-      <View style={styles.header}>
-        <DrawerToggleButton />
-      </View>
-
+      
       <TouchableOpacity style={styles.button} onPress={signUp}>
         <Text style={styles.label}>Criar Conta</Text>
       </TouchableOpacity>
@@ -24,9 +20,9 @@ export default function Index(){
 const styles = StyleSheet.create({
   constainer: { 
     flex: 1, 
+    justifyContent: "center",
     alignItems: "center",
     gap: 32,
-    padding: 32,
   },
   label: {
     fontSize: 16,
@@ -39,9 +35,4 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     borderRadius: 10,
   },
-  header: {
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: "flex-end",
-  }
 })
