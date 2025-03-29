c803197e289a7b991e72130e9cc5f07be7991745
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-03-29 20:12:05 -0300

    Tabs e Drawer - Customizando Drawer

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,36 @@
 import { Drawer } from 'expo-router/drawer'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { MaterialIcons } from '@expo/vector-icons'
 
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-       <Drawer />
+      <Drawer screenOptions={{
+        drawerActiveTintColor: "#000000",
+        drawerInactiveTintColor: "#999999"
+      }}>
+       <Drawer.Screen 
+        name='index' 
+        options={{
+          title: "Entrar",
+          drawerLabel: "Entrar",
+          drawerIcon: ({ color, size }) => (
+            <MaterialIcons name='home' size={size} color={color} />
+          ),
+        }}
+        />
+
+        <Drawer.Screen 
+          name="sign-up"
+          options={{
+            title: "Criar Conta",
+            drawerLabel: "Criar Conta",
+            drawerIcon: ({ color, size }) => (
+              <MaterialIcons name='add' size={size} color={color} />
+            ),
+          }}
+          />
+      </Drawer>
      </GestureHandlerRootView>
   )
 }
